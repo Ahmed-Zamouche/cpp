65eabcb55cb8caddb3d60ebaef2cbc6c2ec587e9
Ahmed Zamouche <ahmed.zamouche@gmail.com>
2020-03-09 12:04:08 +0100

    Update variadic_0.cpp

--- tmp/variadic_0.cpp
+++ tmp/variadic_0.cpp
@@ -1,28 +1,29 @@
-#include <type_traits>
 #include <iostream>
+#include <type_traits>
+#include <vector>
 #define NDEBUG
 
-template<bool lsb>
-int reversed_binary_value()
-{
+template <bool a_0>
+int reversed_binary_value() {
 #ifndef NDEBUG
-    std::cout << __PRETTY_FUNCTION__ << ": " << lsb << std::endl;
+  std::cout << __PRETTY_FUNCTION__ << ": " << a_0 << std::endl;
 #endif
-    return lsb;
+  return a_0;
 }
 
-template<bool a, bool b, bool... digits>
-int reversed_binary_value()
-{
+template <bool a_n_minus_1, bool a_n_minus_2, bool... digits>
+int reversed_binary_value() {
 #ifndef NDEBUG
-    std::cout << __PRETTY_FUNCTION__ << ": " <<  sizeof...(digits) << std::endl;
+  std::cout << __PRETTY_FUNCTION__ << ": " << sizeof...(digits) << std::endl;
 #endif
 
-    return  a * (1 << (sizeof...(digits) + 1) ) +  reversed_binary_value<b, digits...>();
+  return a_n_minus_1 * (1 << (sizeof...(digits) + 1)) +
+         reversed_binary_value<a_n_minus_2, digits...>();
 }
 
 using namespace std;
 int main() {
-    cout << reversed_binary_value<1, 0, 1>() << endl;
+  cout << reversed_binary_value<1, 0, 1>() << endl;
   return 0;
 }
+
